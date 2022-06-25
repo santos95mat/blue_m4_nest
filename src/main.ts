f09d1f3b1ac7e0a289aa3d3375808e21ea474063
matheus <santos95.mat@gmail.com>
2022-06-24 21:05:17 -0300

    feature: create class validation and swagger

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,24 @@
+import { ValidationPipe } from '@nestjs/common';
 import { NestFactory } from '@nestjs/core';
+import { SwaggerModule, DocumentBuilder } from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.useGlobalPipes(new ValidationPipe());
+
+  const config = new DocumentBuilder()
+    .setTitle('API NEST')
+    .setDescription('API criada em Nest.JS para fins de aprendizado')
+    .setVersion('1.0')
+    .addTag('status')
+    .addTag('users')
+    .build();
+
+  const document = SwaggerModule.createDocument(app, config);
+
+  SwaggerModule.setup('docs', app, document);
 
   await app.listen(3333);
 }
--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,8 +1,10 @@
 import { Body, Controller, Get, Post } from '@nestjs/common';
+import { ApiTags } from '@nestjs/swagger';
 import { CreateUserDto } from './dto/create-user.dto';
 import { User } from './entities/users.entities';
 import { UsersService } from './users.service';
 
+@ApiTags('users')
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
